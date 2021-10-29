573d917d1b5898b081eb496b0b845c0b0bc403ad
Kasimir Schulz <kasimirkhschulz@gmail.com>
2021-10-29 15:25:24 -0400

    Changed types in the headers to correctly display numbers

--- types.h
+++ types.h
@@ -1,17 +1,17 @@
-struct x8XPHEADER {
-    char magic[0x8];            // Magic number "**TI83F*"
-    char second_signature[0x3]; // Second signature (Always 1A 0A 00)
-    char comments[0x2a];        // Comments section
-    char data_len[0x2];         // Length of code and code header
+struct x8XPHEADER __packed {
+    int8_t magic[0x8];            // Magic number "**TI83F*"
+    int8_t second_signature[0x3]; // Second signature (Always 1A 0A 00)
+    int8_t comments[0x2a];        // Comments section
+    int16_t data_len;         // Length of code and code header
 };
 
-struct VARIABLE_ENTRY_HEADER {
-    char entry_start[0x2];      // Offset to start of data in entry (Always 0B or 0D), for the program the data still has some header components
-    char program_size[0x2];     // Program Size
-    char entry_id[0x1];         // Variable type ID byte
-    char filename[0x8];         // Filename
-    char version[0x1];          // Version information
-    char flag[0x1];             // Flag, set to 0x80 if entry is archived
-    char entry_len[0x2];        // Length of the entry
-    char code_len[0x2];         // This is in the data technically but is the length of the code
+struct VARIABLE_ENTRY_HEADER __packed {
+    int16_t entry_start;      // Offset to start of data in entry (Always 0B or 0D), for the program the data still has some header components
+    int16_t program_size;     // Program Size
+    int8_t entry_id;         // Variable type ID byte
+    int8_t filename[0x8];         // Filename
+    int8_t version;          // Version information
+    int8_t flag;             // Flag, set to 0x80 if entry is archived
+    int16_t entry_len;        // Length of the entry
+    int16_t code_len;         // This is in the data technically but is the length of the code
 };
